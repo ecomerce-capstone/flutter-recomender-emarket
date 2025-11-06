fb262f77ac174a9863bba5c4eb9ca8cd6e24df5d
Indra Suryawan <ecomercecapstone@gmail.com>
2025-11-06 13:55:04 +0700

    update widget product list tile

--- lib/widgets/product_list_tile.dart
+++ lib/widgets/product_list_tile.dart
@@ -4,11 +4,13 @@ class ProductListTile extends StatelessWidget {
   final String name;
   final String subtitle;
   final String trailing;
-  ProductListTile({
+  const ProductListTile({
     required this.name,
     required this.subtitle,
     required this.trailing,
-  });
+    Key? key,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
